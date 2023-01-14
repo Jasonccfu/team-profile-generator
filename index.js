1a44527ac90084cf054fc4646d691dcf4e652acf
Jason Chen <jason.ccfu@gmail.com>
2023-01-14 01:16:29 -0800

    update constructors

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // Input packages
 const fs = require("fs");
 const inquirer = requirer("inquirer");
+const 
 
-// set the feld for generaters
-
+// set for generaters
 const generateHtml = require("./src/generateHtmlPage");
+
+//team members profiles
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+
+
