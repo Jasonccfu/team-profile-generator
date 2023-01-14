416cf9c628750b9af62d46cb270a0c236f8eb106
Jason Chen <jason.ccfu@gmail.com>
2023-01-14 03:17:19 -0800

    added new function of addManager

--- index.js
+++ index.js
@@ -1,14 +1,82 @@
 // Input packages
 const fs = require("fs");
-const inquirer = requirer("inquirer");
-const 
+const inquirer = require('inquirer');
 
-// set for generaters
+// Set for generaters
 const generateHtml = require("./src/generateHtmlPage");
 
-//team members profiles
-const manager = require('./lib/manager');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
+// Team members profiles
+const manager = require('./lib/Manager');
+const engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+
+// Team array
+// (Array to push the prompts results)
+const teamArray = [];
+
+// Start manager prompts first 
+// (Must have one manager in the team)
+const addManager = () =>{
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name:'name',
+            message:"Please enter manager's name ",
+            validate: nameInput => {
+                if (nameInput){
+                    return true
+                } else {
+                    console.log("Please enter the manager's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name:'id',
+            message:'Please enter ID number ',
+            validate: idInput => {
+                if (idInput){
+                    return true
+                } else {
+                    console.log("Please enter the manager's id.");
+                    return false;
+                }
+            }
+        },{
+            type:'input',
+            name:'email',
+            message:"Please enter the manager's email address",
+            validate: emailInput => {
+                if(emailInput){
+                    return true
+                } else {
+                    console.log("Please enter the manager's email address")
+                    return false;
+                }
+
+            }
+        },
+        {
+            type: 'input',
+            name:'officeNumber',
+            message:'Please enter office number',
+            validate: officeNumberInput => {
+                if (officeNumberInput){
+                    return true
+                } else {
+                    console.log("Please enter the office number.");
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(answers => {
+        console.log(answers);
+
+    })
+}; //end addManager
+
+addManager();
 
 
